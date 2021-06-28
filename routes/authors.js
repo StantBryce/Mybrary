cc28e6c8b6f0c2b242563448096d6c89d20cbf97
StantBryce <78883702+StantBryce@users.noreply.github.com>
2021-06-28 13:54:13 -0700

    fixed author error

--- routes/authors.js
+++ routes/authors.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const Author = require('../models/author');
 const router = express.Router();
@@ -5,14 +6,14 @@ const router = express.Router();
 // All authors route
 router.get('/', async (req, res) => {
     let searchOptions = {};
-    if(req.query.name !== null && req.query.name !== '') {
+    if (req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i');
     }
     try {
         const authors = await Author.find(searchOptions);
         res.render('authors/index', {
             authors: authors,
-            searchOptions: req.query.name 
+            searchOptions: req.query
         });
     } catch {
         res.redirect('/');
@@ -42,3 +43,50 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
+/*
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
+})
+
+// Create Author Route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
+    })
+  }
+})
+
+module.exports = router
+*/
